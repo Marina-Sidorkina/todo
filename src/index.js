c59a54b3d733ce1698efb28c6a75ca182ab817d1
Marina Sidorkina <m.sidorkina55@gmail.com>
2020-10-24 02:26:23 +0300

    Add components. Fix project structure.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const TodoList = () => {
-  return (
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-}
-
-const SearchPanel = () => {
-  return <input placeholder="search"/>;
-}
-
-const App = () => {
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-}
+import App from './components/app/'
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));

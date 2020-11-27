a96e012632e7c49d54172ef5e0a486c701a0df1b
Marina Sidorkina <m.sidorkina55@gmail.com>
2020-11-27 20:11:02 +0300

    Change functional app component to class.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header/';
 import SearchPanel from '../search-panel/';
 import TodoList from '../todo-list/';
 import ItemStatusFilter from '..//item-status-filter/';
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Learn React', important: true, id: 2 },
-    { label: 'Take a Walk', important: false, id: 3 }
-  ];
+export default class App extends Component = () => {
+  constructor() {
+    super();
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+    this.state = {
+      todoData: [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Learn React', important: true, id: 2 },
+        { label: 'Take a Walk', important: false, id: 3 }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList 
+          todos={this.state.todoData}
+          onDeleted={(id) => console.log('del', id)}
+        />
       </div>
-
-      <TodoList 
-        todos={todoData}
-        onDeleted={(id) => console.log('del', id)}
-      />
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
